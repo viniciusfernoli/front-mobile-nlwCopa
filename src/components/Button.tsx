46b92e99a58ecf2a30d9a53f3344696ed9acf2bd
Vinicius Fernandes <vinicius.fernandes@bravolog.com.br>
2022-11-04 16:54:01 -0300

    tela pronta, porém sem funcionalidade o botão de conectar ao google

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import {Button as ButtonNativeBase, Text, IButtonProps} from 'native-base'
+
+interface ButtonProps extends IButtonProps {
+  title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
+}
+
+export function Button({title, type="PRIMARY", ...rest}:ButtonProps) {
+  return(
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      rounded="sm" 
+      textTransform="uppercase"
+      bg={type==='SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type==='SECONDARY' ? 'red.600' : 'yellow.600'
+      }}
+      _loading={{
+        _spinner: {color: 'black'}
+      }}
+       {...rest}>
+      <Text
+        fontFamily='heading'
+        fontSize='sm'
+        color={type==='SECONDARY' ? 'white' : 'black'}
+      >
+        {title}</Text>
+    </ButtonNativeBase>
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,28 @@
-import {Center, Text} from "native-base"
+import {Center, Text, Icon} from "native-base";
+import Logo from '../assets/logo.svg';
+import {Fontisto} from '@expo/vector-icons'
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily='heading'>
-        SignIn
-      </Text>
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button 
+        title="Entrar com o Google"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name='google' color="white" size="md" mr="3"/>}
+        color="white"
+        mt="12"
+        fontSize="sm"
+      />
+      <Text 
+        fontSize="sm" 
+        textAlign="center" 
+        color="gray.200" 
+        fontFamily='heading'
+        mt="4"
+      >
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.</Text>
     </Center>
   );
 }
